eb3ebe5d216ae186a3cd2645e4f026e13bf54165
Antikrizisss <78258378+Antikrizisss@users.noreply.github.com>
2021-04-05 11:25:53 +0400

    Добавил обработку исключений при вводе значений

--- new1/Ekran.cs
+++ new1/Ekran.cs
@@ -12,8 +12,17 @@ namespace new1
         {
             get
             {
-                Console.WriteLine("Введите длину двух катетов (целых или с плавающей точкой) последовательно через Enter ");
-                return Convert.ToDouble(Console.ReadLine());
+                try
+                {
+                    Console.WriteLine("Введите длину двух катетов (целых или с плавающей точкой) последовательно через Enter ");
+                    return Convert.ToDouble(Console.ReadLine());
+                }
+                catch (SystemException)
+                {
+                    Console.WriteLine("Можно вводить только числа (4 или 4,3)");
+                    Console.WriteLine("Введите длину еще раз: ");
+                    return Convert.ToDouble(Console.ReadLine());
+                }
             }
         }
 
@@ -21,8 +30,17 @@ namespace new1
         {
             get
             {
-                Console.WriteLine();
-                return Convert.ToDouble(Console.ReadLine());
+                try
+                {
+                    Console.WriteLine("Вторая длина: ");
+                    return Convert.ToDouble(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Можно вводить только числа (например 4 или 4,3)");
+                    Console.WriteLine("Введите длину еще раз: ");
+                    return Convert.ToDouble(Console.ReadLine());
+                }
             }
         }
         public static double VichislimGipotenuzu(double x, double y)
--- new1/Gipotenuza.cs
+++ new1/Gipotenuza.cs
@@ -8,43 +8,6 @@ namespace new1
 {
     class Gipotenuza
     {
-        /*
-        double katet1;
-        double katet2;
-
-        public double Katet1
-        {
-            get => katet1;
-            set
-            {
-                if (value <= 0)
-                {
-                    return;
-                }
-                else
-                {
-                    katet1 = value;
-                }
-            }
-        }
-
-        public double Katet2
-        {
-            get => katet2;
-            set
-            {
-                if (value <= 0)
-                {
-                    return;
-                }
-                else
-                {
-                    katet2 = value;
-                }
-            }
-        }
-
-        */
         public double VichislimGipotenuzu(double x, double y)
         {
             return Math.Sqrt(x * x + y * y);
